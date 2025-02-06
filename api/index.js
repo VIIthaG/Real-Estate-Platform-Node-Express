3f160fe567a89ea0153c5978d96da06528d284e1
VIIthaG <vllthag@gmail.com>
2025-02-06 08:20:36 +0530

    Connecting to Mongo Atlas

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Datebase Connected"))
+  .catch((err) => console.error(err));
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`listening to port ${PORT}`);
-});
+
+const connec = async () => {
+  await app.listen(PORT, () => {
+    console.log(`listening to port ${PORT}`);
+  });
+};
