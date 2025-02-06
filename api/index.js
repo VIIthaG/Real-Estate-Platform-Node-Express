2e78f7e7275aea18800ab41f8adfa8706ad88b7d
VIIthaG <vllthag@gmail.com>
2025-02-06 09:08:58 +0530

    Set up basic MVC

--- api/index.js
+++ api/index.js
@@ -2,18 +2,23 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+import userRoute from "./routes/user.routes.js";
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("Datebase Connected"))
+  .then(() => console.log("Database Connected"))
   .catch((err) => console.error(err));
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const connec = async () => {
   await app.listen(PORT, () => {
     console.log(`listening to port ${PORT}`);
   });
 };
+
+connec();
+
+app.use("/api/user", userRoute);
