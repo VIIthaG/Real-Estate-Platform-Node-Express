ddf5f2009096ec500af9c06d0c49bc2b66f7d64d
VIIthaG <vllthag@gmail.com>
2025-03-03 11:20:41 +0530

    Fixed bug that did'nt cause proper deletion of listings on rerender.

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -13,14 +13,17 @@ export const createListing = async (req, res, next) => {
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
-  if (!listing) return next(errorHandler(404, "Listing Not Found"));
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
 
-  if (req.user.id !== listing.userRef)
-    return next(errorHandler(401, "You can delete Your Own Listings"));
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only delete your own listings!"));
+  }
+
   try {
-    Listing.findOneAndDelete(req.params.id);
-
-    return res.status(201).json("Deleted Successfully");
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted!");
   } catch (error) {
     next(error);
   }
